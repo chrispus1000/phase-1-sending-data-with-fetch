ef1cf0f1c9fc84400ace936aa08ae760ecc8354b
chrispus1000 <minrevac@gmail.com>
2022-06-17 20:12:05 +0300

    commit tree 0443f0ab427d54d69243084f7223816659b16ae1

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+function submitData(userName, userEmail) {
+    const userObject = {name: userName, email: userEmail};
+    return fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+        },
+        body: JSON.stringify(userObject)
+    })
+    .then(res => res.json())
+    .then(data => appendData(data))
+    .catch(error => appendError(error));
+
+}
+
+function appendData(data) {
+    const body = document.querySelector('body');
+    body.innerHTML = data.id;
+}
+
+function appendError(error) {
+    const body = document.querySelector('body');
+    body.innerHTML = error;
+}
